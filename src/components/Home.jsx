73e80059b266286f89f425e2b4636d4f364c9bb3
vahsimonian <simonianvah@gmail.com>
2023-12-01 19:54:39 +0700

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./home.css";
+import juiceData from '../juiceData'
 
 function Home() {
-  return <div></div>;
+
+  const juiceItem = juiceData.map((juice) => (
+
+  ))
+
+  return <div id="home">
+
+  </div>;
 }
 
 export default Home;
