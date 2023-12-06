1461d4774babc6da24c67296e1786a6acafeb4bd
vahsimonian <simonianvah@gmail.com>
2023-12-06 12:25:16 +0700

    commit

--- src/juiceData.js
+++ src/juiceData.js
@@ -1,4 +1,34 @@
-//  import redDelight from "../src/assets/";
+import redDelight from "../src/assets/Red Delight.png";
+import applePearMedley from "../src/assets/Apple Pear Medley.png";
+import autumnHarves from "../src/assets/Autumn Harves.png";
+import autumnHarvestDelight from "../src/assets/Autumn Harvest.png";
+import beetFusion from "../src/assets/Beet Fusion.png";
+import berryPurple from "../src/assets/Berry Purple.png";
+import citrusSurge from "../src/assets/Citrus Surge.png";
+import coolingBreeze from "../src/assets/Cooling Breeze.png";
+import cranAppleSplash from "../src/assets/Cran-Apple Splash.png";
+import currantBlueFusion from "../src/assets/Currant Blue Fusion.png";
+import dragonWatermelonBurst from "../src/assets/Dragon Watermelon Burst.png";
+import greenDetox from "../src/assets/Green Detox.png";
+import greenPower from "../src/assets/Green Power.png";
+import greenRefresh from "../src/assets/Green Refresh.png";
+import greenZest from "../src/assets/Green Zest.png";
+import lemonPearFusion from "../src/assets/Lemon Pear Fusion.png";
+import melonDragonFusion from "../src/assets/Melon Dragon Fusion.png";
+import mintyFreshness from "../src/assets/Minty Freshness.png";
+import orangeBoost from "../src/assets/Orange Boost.png";
+import pinkParadise from "../src/assets/Pink Paradise.png";
+import purplePotion from "../src/assets/Purple Potion.png";
+import purpleSweetness from "../src/assets/Purple Sweetness.png";
+import rubyRed from "../src/assets/Ruby Red.png";
+import savoryRed from "../src/assets/Savory Red.png";
+import sunriseBlend from "../src/assets/Sunrise Blend.png";
+import sunsetGlow from "../src/assets/Sunset Glow.png";
+import tropicalSmooth from "../src/assets/Tropical Smooth.png";
+import tropicalSmoothie from "../src/assets/Tropical Smoothie.png";
+import tropicalSunrise from "../src/assets/Tropical Sunrise.png";
+import zestyLemonade from "../src/assets/Zesty Lemonade.png";
+import all from "../src/assets/all.png";
 
 export const juiceData = [
   {
@@ -10,6 +40,7 @@ export const juiceData = [
     description: "A sweet blend of apples and strawberries.",
     price: "$3.99",
     available: true,
+    img: redDelight,
   },
   {
     id: 2,
@@ -20,6 +51,7 @@ export const juiceData = [
     description: "Tart and refreshing with kiwi and a hint of lime.",
     price: "$4.50",
     available: true,
+    img: greenZest,
   },
   {
     id: 3,
@@ -30,6 +62,7 @@ export const juiceData = [
     description: "A tropical blend of ripe pineapple and mango.",
     price: "$4.25",
     available: true,
+    img: tropicalSunrise,
   },
   {
     id: 4,
@@ -40,6 +73,7 @@ export const juiceData = [
     description: "Rich in antioxidants with blueberries and grapes.",
     price: "$5.00",
     available: true,
+    img: purplePotion,
   },
   {
     id: 5,
@@ -50,6 +84,7 @@ export const juiceData = [
     description: "Vitamin-rich blend of orange and carrot.",
     price: "$3.75",
     available: true,
+    img: orangeBoost,
   },
   {
     id: 6,
@@ -60,6 +95,7 @@ export const juiceData = [
     description: "A refreshing mix of guava and strawberries.",
     price: "$4.00",
     available: true,
+    img: pinkParadise,
   },
   {
     id: 7,
@@ -70,6 +106,7 @@ export const juiceData = [
     description: "A sweet and tangy blend of peach and orange.",
     price: "$3.99",
     available: true,
+    img: sunsetGlow,
   },
   {
     id: 8,
@@ -80,6 +117,7 @@ export const juiceData = [
     description: "A cool and crisp apple-cucumber mix.",
     price: "$4.20",
     available: true,
+    img: greenRefresh,
   },
   {
     id: 9,
@@ -90,6 +128,7 @@ export const juiceData = [
     description: "A zingy lemon and ginger blend.",
     price: "$3.80",
     available: true,
+    img: zestyLemonade,
   },
   {
     id: 10,
@@ -100,6 +139,7 @@ export const juiceData = [
     description: "A rich blend of cherry and pomegranate.",
     price: "$4.50",
     available: true,
+    img: rubyRed,
   },
 
   {
@@ -111,6 +151,7 @@ export const juiceData = [
     description: "Exotic dragonfruit blended with sweet watermelon.",
     price: "$4.75",
     available: true,
+    img: dragonWatermelonBurst,
   },
   {
     id: 12,
@@ -121,6 +162,7 @@ export const juiceData = [
     description: "Cool and refreshing mint with a touch of cucumber.",
     price: "$3.50",
     available: true,
+    img: mintyFreshness,
   },
   {
     id: 13,
@@ -131,6 +173,7 @@ export const juiceData = [
     description: "A mix of tart raspberry and earthy beet.",
     price: "$4.00",
     available: true,
+    img: beetFusion,
   },
   {
     id: 14,
@@ -141,6 +184,7 @@ export const juiceData = [
     description: "Sweet blend of banana and pineapple for a tropical taste.",
     price: "$3.95",
     available: true,
+    img: tropicalSmooth,
   },
   {
     id: 15,
@@ -151,6 +195,7 @@ export const juiceData = [
     description: "Rich and deep flavors of blackcurrant and blueberry.",
     price: "$4.50",
     available: true,
+    img: berryPurple,
   },
   {
     id: 16,
@@ -161,6 +206,7 @@ export const juiceData = [
     description: "A hearty blend of pumpkin and carrot, perfect for fall.",
     price: "$4.25",
     available: true,
+    img: autumnHarvestDelight,
   },
   {
     id: 17,
@@ -171,6 +217,7 @@ export const juiceData = [
     description: "Healthy kale mixed with sweet green apple.",
     price: "$4.75",
     available: true,
+    img: greenDetox,
   },
   {
     id: 18,
@@ -181,6 +228,7 @@ export const juiceData = [
     description: "Unique blend of tomato and red pepper for a savory twist.",
     price: "$3.80",
     available: true,
+    img: savoryRed,
   },
   {
     id: 19,
@@ -191,6 +239,7 @@ export const juiceData = [
     description: "A subtle mix of pear with a hint of lemon.",
     price: "$4.00",
     available: true,
+    img: lemonPearFusion,
   },
   {
     id: 20,
@@ -201,6 +250,7 @@ export const juiceData = [
     description: "Sweet and rich blend of grapes and plums.",
     price: "$4.50",
     available: true,
+    img: purpleSweetness,
   },
   {
     id: 21,
@@ -211,6 +261,7 @@ export const juiceData = [
     description: "Nutrient-packed blend of avocado and spinach.",
     price: "$5.00",
     available: true,
+    img: greenPower,
   },
   {
     id: 22,
@@ -221,6 +272,7 @@ export const juiceData = [
     description: "A vibrant mix of mango and carrot.",
     price: "$4.20",
     available: true,
+    img: sunriseBlend,
   },
 
   {
@@ -232,6 +284,7 @@ export const juiceData = [
     description: "Crisp and refreshing blend of green apple and pear.",
     price: "$3.95",
     available: true,
+    img: applePearMedley,
   },
   {
     id: 24,
@@ -242,6 +295,7 @@ export const juiceData = [
     description: "Tart cranberry meets sweet apple in this vibrant blend.",
     price: "$4.25",
     available: true,
+    img: cranAppleSplash,
   },
   {
     id: 25,
@@ -252,6 +306,7 @@ export const juiceData = [
     description: "A deep, rich blend of blackcurrant and blueberry.",
     price: "$4.75",
     available: true,
+    img: currantBlueFusion,
   },
   {
     id: 26,
@@ -262,6 +317,7 @@ export const juiceData = [
     description: "A unique blend of carrot and pumpkin for a fall flavor.",
     price: "$3.85",
     available: true,
+    img: autumnHarves,
   },
   {
     id: 27,
@@ -272,6 +328,7 @@ export const juiceData = [
     description: "Exotic dragonfruit mixed with sweet watermelon.",
     price: "$5.00",
     available: true,
+    img: melonDragonFusion,
   },
   {
     id: 28,
@@ -282,6 +339,7 @@ export const juiceData = [
     description: "A refreshing and cooling blend of cucumber and mint.",
     price: "$3.50",
     available: true,
+    img: coolingBreeze,
   },
   {
     id: 29,
@@ -292,6 +350,7 @@ export const juiceData = [
     description: "A tangy and invigorating mix of blood orange and grapefruit.",
     price: "$4.50",
     available: true,
+    img: citrusSurge,
   },
   {
     id: 30,
@@ -302,5 +361,6 @@ export const juiceData = [
     description: "A creamy blend of pineapple and banana for a tropical treat.",
     price: "$4.00",
     available: true,
+    img: tropicalSmoothie,
   },
 ];
