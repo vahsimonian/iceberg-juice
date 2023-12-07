1aabc77e93fd09c49beec86ff545f9af23e081ca
vahsimonian <simonianvah@gmail.com>
2023-12-07 10:37:34 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,54 +5,55 @@ import { Link } from "react-scroll";
 function Navbar() {
   return (
     <nav id="navbar">
-      <div className="letter0-v">
-        <img src="" alt="" />
-        <div className="nav-link-container">
-          <Link
-            activeClass="active"
-            to="home"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            classID="nav-link-item"
-          >
-            Home
-          </Link>
-          <Link
-            activeClass="active"
-            to="products"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            classID="nav-link-item"
-          >
-            About me
-          </Link>
-          <Link
-            activeClass="active"
-            to="order"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            classID="nav-link-item"
-          >
-            My works
-          </Link>
-          <Link
-            activeClass="contact"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            classID="nav-link-item"
-          >
-            Contact
-          </Link>
-        </div>
+      <div className="nav-link-container">
+        <Link
+          className="navbar-link-item"
+          activeClass="active"
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          classID="nav-link-item"
+        >
+          Home
+        </Link>
+        <Link
+          className="navbar-link-item"
+          activeClass="active"
+          to="products"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          classID="nav-link-item"
+        >
+          About me
+        </Link>
+        <Link
+          className="navbar-link-item"
+          activeClass="active"
+          to="order"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          classID="nav-link-item"
+        >
+          My works
+        </Link>
+        <Link
+          className="navbar-link-item"
+          activeClass="contact"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          classID="nav-link-item"
+        >
+          Contact
+        </Link>
       </div>
     </nav>
   );
