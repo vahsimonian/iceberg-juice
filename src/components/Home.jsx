293db330ed8ff7bc00b5fa491380fde7ccdfcc67
vahsimonian <simonianvah@gmail.com>
2023-12-09 13:16:28 +0700

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,8 @@
 import "./home.css";
 import React from "react";
-import { RiShoppingCartFill } from "react-icons/ri";
-import { RiShoppingCartLine } from "react-icons/ri";
 
 function Home() {
-  return (
-    <div>
-      <RiShoppingCartLine />
-      <RiShoppingCartFill color="red" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
--- src/components/Juices.jsx
+++ src/components/Juices.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import "./juices.css";
 import { juiceData } from "../juiceData";
+import { RiShoppingCartFill } from "react-icons/ri";
+import { RiShoppingCartLine } from "react-icons/ri";
 
 function Juices() {
   const [hovered, setHovered] = useState(false);
+  const [filledInShop, setFilledInShop] = useState(false);
 
   const orderButton = hovered ? "none" : "block";
+
+  const shoppingCartToggle = filledInShop ? (
+    <RiShoppingCartFill />
+  ) : (
+    <RiShoppingCartLine />
+  );
 
   const juiceItem = juiceData.map((juice) => {
     return (
@@ -18,15 +27,15 @@ function Juices() {
         {/* <button className="orderButton"></button> */}
         <h2 style={{ backgroundColor: juice.color }}>{juice.title}</h2>
         <img src={juice.img} alt="juice" />
-
-        <div
-          className="color-overlay"
-          style={{ backgroundColor: juice.color }}
-        ></div>
-        <div className="juice-p">
-          <p>{juice.price}</p>
-          <p>{juice.fruits.join(" + ")}</p>
-          <p>{juice.description}</p>
+        <div className="color-overlay" style={{ backgroundColor: juice.color }}>
+          <div onClick={() => setFilledInShop(!filledInShop)}>
+            {shoppingCartToggle}
+          </div>
+          <div className="juice-p">
+            <p>{juice.price}</p>
+            <p>{juice.fruits.join(" + ")}</p>
+            <p>{juice.description}</p>
+          </div>
         </div>
       </div>
     );
