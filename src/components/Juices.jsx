2f38a0be34f4ea9086fefa8f29852579abb5cfb0
vahsimonian <simonianvah@gmail.com>
2023-12-09 11:14:16 +0700

    commit

--- src/components/Juices.jsx
+++ src/components/Juices.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./juices.css";
 import { juiceData } from "../juiceData";
 
 function Juices() {
+  const [hovered, setHovered] = useState(false);
+
+  const orderButton = hovered ? "none" : "block";
+
   const juiceItem = juiceData.map((juice) => {
     return (
-      <div className="juice-item" style={{ backgroundColor: "white" }}>
+      <div
+        className="juice-item"
+        style={{ backgroundColor: "white" }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(true)}
+      >
+        {/* <button className="orderButton"></button> */}
         <h2 style={{ backgroundColor: juice.color }}>{juice.title}</h2>
         <img src={juice.img} alt="juice" />
-        {/* <button className="button"> */}
+
         <div
           className="color-overlay"
           style={{ backgroundColor: juice.color }}
         ></div>
-        {/* </button> */}
         <div className="juice-p">
           <p>{juice.price}</p>
           <p>{juice.fruits.join(" + ")}</p>
