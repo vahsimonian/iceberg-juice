1678b4c7aacedac8357b93849a7a6b8c0c46a0ff
vahsimonian <simonianvah@gmail.com>
2023-12-10 19:05:49 +0700

    commit

--- src/components/Juices.jsx
+++ src/components/Juices.jsx
@@ -2,13 +2,34 @@ import React, { useState } from "react";
 import "./juices.css";
 import { juiceData } from "../juiceData";
 import { RiShoppingCartFill, RiShoppingCartLine } from "react-icons/ri";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { IoInformationCircleOutline } from "react-icons/io5";
 
 function Juices() {
   const [filledInShop, setFilledInShop] = useState({});
+  const [favorites, setFavorites] = useState({});
+  const [hovered, setHovered] = useState({});
 
   const juiceItem = juiceData.map((juice) => {
     const isFilled = filledInShop[juice.id];
     const iconClass = `shopping-cart-icon ${isFilled ? "filled" : ""}`;
+
+    const isFavorite = favorites[juice.id];
+    const favoriteIconClass = `favorite-icon ${isFavorite ? "filled" : ""}`;
+
+    const infoIcon = () => {
+      setHovered((prevState) => ({
+        ...prevState,
+        [juice.id]: !prevState[juice.id],
+      }));
+    };
+
+    const toggleFavorite = () => {
+      setFavorites((prevState) => ({
+        ...prevState,
+        [juice.id]: !prevState[juice.id],
+      }));
+    };
 
     const toggleFilledInShop = () => {
       setFilledInShop((prevState) => ({
@@ -24,11 +45,23 @@ function Juices() {
         <div className="color-overlay" style={{ backgroundColor: juice.color }}>
           <div onClick={toggleFilledInShop} className={iconClass}>
             {isFilled ? (
-              <RiShoppingCartFill style={{ color: juice.color }} />
+              <RiShoppingCartFill style={{ color: "white" }} />
             ) : (
-              <RiShoppingCartLine />
+              <RiShoppingCartLine style={{ color: "white" }} />
             )}
           </div>
+          <div onClick={toggleFavorite} className={favoriteIconClass}>
+            {isFavorite ? (
+              <AiFillHeart style={{ color: "white" }} />
+            ) : (
+              <AiOutlineHeart style={{ color: "white" }} />
+            )}
+          </div>
+          {hovered && (
+            <div className="info-icon" onClick={infoIcon}>
+              <IoInformationCircleOutline />
+            </div>
+          )}
           <div className="juice-p">
             <p>{juice.price}</p>
             <p>{juice.fruits.join(" + ")}</p>
