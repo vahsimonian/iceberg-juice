dcb8ec854b865e6ac13a62063e0a647df9f1d24f
vahsimonian <simonianvah@gmail.com>
2023-12-15 00:48:16 +0700

    commit

--- src/components/Juices.jsx
+++ src/components/Juices.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import "./juices.css";
 import { juiceData, fourJuicesCircled } from "../juiceData";
 import { RiShoppingCartFill, RiShoppingCartLine } from "react-icons/ri";
