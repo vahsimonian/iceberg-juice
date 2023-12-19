efacf4383c049bf9fdfa45df909e26ef08bd1234
vahsimonian <simonianvah@gmail.com>
2023-12-19 23:16:58 +0700

    commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./contact.css";
+import { RiInstagramFill } from "react-icons/ri";
+// import { RiInstagramLine } from "react-icons/ri";
+import { RiFacebookCircleFill } from "react-icons/ri";
+// import { RiFacebookCircleLine } from "react-icons/ri";
+import { RxLinkedinLogo } from "react-icons/rx";
+// import { PiLinkedinLogo } from "react-icons/pi";
 
 function Contact() {
-  return <div id="contact">Contact</div>;
+  return (
+    <div id="contact">
+      <RiInstagramFill className="insgram-icon icons" />
+      {/* <RiInstagramLine className="insgram-icon" /> */}
+      <RiFacebookCircleFill className="icons" />
+      {/* <RiFacebookCircleLine /> */}
+      <RxLinkedinLogo className="icons" />
+      {/* <PiLinkedinLogo className="icons" /> */}
+    </div>
+  );
 }
 
 export default Contact;
