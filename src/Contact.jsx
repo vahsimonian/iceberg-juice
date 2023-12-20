107e4eaaed30792f3f539a212cca68e15b1763f5
vahsimonian <simonianvah@gmail.com>
2023-12-20 20:22:36 +0700

    commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import "./contact.css";
 import { RiInstagramFill } from "react-icons/ri";
-// import { RiInstagramLine } from "react-icons/ri";
-import { RiFacebookCircleFill } from "react-icons/ri";
-// import { RiFacebookCircleLine } from "react-icons/ri";
 import { RxLinkedinLogo } from "react-icons/rx";
-// import { PiLinkedinLogo } from "react-icons/pi";
+import { GrMail } from "react-icons/gr";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { CgProfile } from "react-icons/cg";
 
 function Contact() {
   return (
     <div id="contact">
-      <RiInstagramFill className="icons" />
-      {/* <RiInstagramLine className="insgram-icon" /> */}
-      <RiFacebookCircleFill className="icons" />
-      {/* <RiFacebookCircleLine /> */}
-      <RxLinkedinLogo className="icons" />
-      {/* <PiLinkedinLogo className="icons" /> */}
+      <RiInstagramFill
+        className="icons"
+        href="https://www.instagram.com/iceberg.kampot/"
+        target="_blank"
+      />
+      <RxLinkedinLogo
+        className="icons"
+        href="https://www.instagram.com/iceberg.kampot/"
+        target="_blank"
+      />
+      <CgProfile
+        className="icons"
+        href="https://www.instagram.com/iceberg.kampot/"
+        target="_blank"
+      />
+      <GrMail
+        className="icons"
+        href="https://www.instagram.com/iceberg.kampot/"
+        target="_blank"
+      />
+      <FaSquareXTwitter
+        className="icons"
+        href="https://www.instagram.com/iceberg.kampot/"
+        target="_blank"
+      />
     </div>
   );
 }
