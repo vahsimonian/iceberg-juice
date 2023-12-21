4b489d258e76cada8a8ec445ab4c7fc2efc198c5
vahsimonian <simonianvah@gmail.com>
2023-12-21 07:52:05 +0700

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,23 +4,26 @@ import React from "react";
 function Home() {
   return (
     <div id="home">
-      <p className="juice-desc">
-        "Iceberg Kampot," a burgeoning juice factory that prides itself on being
-        at the heart of Cambodia's famed Kampot region. This factory has rapidly
-        gained a reputation for producing some of the most refreshing and
-        high-quality juices, which has not only captivated a large customer base
-        within Cambodia but has also extended its reach across borders to
-        neighboring countries such as Vietnam, Thailand, and Laos. The growth of
-        "Iceberg Kampot" is a testament to its commitment to quality and
-        customer satisfaction. Reflecting modern trends and the needs of its
-        diverse clientele, the factory has embraced technological advancements,
-        offering the convenience of online payments. This feature ensures that a
-        sip of their natural goodness is just a few clicks away, no matter where
-        you are. Whether you're a local or international customer, the "Home"
-        component invites you to explore "Iceberg Kampot's" offerings and
-        experience the ease of bringing a taste of Cambodia right to your
-        doorstep.
-      </p>
+      <div className="desc-container">
+        <h1 className="home-title">Iceberg Kampot</h1>
+        <p className="juice-desc">
+          "Iceberg Kampot," a burgeoning juice factory that prides itself on
+          being at the heart of Cambodia's famed Kampot region. This factory has
+          rapidly gained a reputation for producing some of the most refreshing
+          and high-quality juices, which has not only captivated a large
+          customer base within Cambodia but has also extended its reach across
+          borders to neighboring countries such as Vietnam, Thailand, and Laos.
+          The growth of "Iceberg Kampot" is a testament to its commitment to
+          quality and customer satisfaction. Reflecting modern trends and the
+          needs of its diverse clientele, the factory has embraced technological
+          advancements, offering the convenience of online payments. This
+          feature ensures that a sip of their natural goodness is just a few
+          clicks away, no matter where you are. Whether you're a local or
+          international customer, the "Home" component invites you to explore
+          "Iceberg Kampot's" offerings and experience the ease of bringing a
+          taste of Cambodia right to your doorstep.
+        </p>
+      </div>
     </div>
   );
 }
