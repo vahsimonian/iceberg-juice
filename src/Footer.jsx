ad731d942777999f4b9f1961b03e78adbabe3fa6
vahsimonian <simonianvah@gmail.com>
2024-01-20 19:13:38 +0700

    commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,25 +3,27 @@ import './footer.css';
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer>
       <div class="footer-content">
-        <section class="footer-section">
+        <section class="footer-section quick-links">
           <h4>Quick Links</h4>
           <ul>
             <li>
-              <a href="/">Terms of Service</a>
+              <a href="/terms">Terms of Service</a>
             </li>
             <li>
-              <a href="/">Privacy Policy</a>
+              <a href="/privacy">Privacy Policy</a>
             </li>
           </ul>
         </section>
-        <section class="footer-section">
+        <section class="footer-section contact-us">
           <h4>Contact Us</h4>
           <p>Email: support@kampotjuice.com</p>
           <p>Phone: +855 123 456 789</p>
         </section>
-        <section class="footer-section">
+      </div>
+      <div class="footer-bottom">
+        <section class="footer-section follow-us">
           <h4>Follow Us</h4>
           <div class="social-icons">
             <a
@@ -40,8 +42,6 @@ function Footer() {
             </a>
           </div>
         </section>
-      </div>
-      <div class="footer-bottom">
         <p>&copy; 2024 Kampot Juice. All rights reserved.</p>
       </div>
     </footer>
