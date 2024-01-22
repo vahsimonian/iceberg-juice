2da3c6022e8cd237a1cd619227e38953ff50f421
vahsimonian <simonianvah@gmail.com>
2024-01-22 22:12:19 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import './App.css';
-import Contact from './Contact';
-import Footer from './Footer';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Juices from './components/Juices';
-import Navbar from './components/Navbar';
-// import Search from "./components/Search";
+import Contact from './Contact';
+import Footer from './Footer';
 import { JuiceProvider } from './JuiceContext';
+// import Search from "./components/Search";
 
 function App() {
   return (
-    <div>
+    <Router>
       <JuiceProvider>
         <Navbar />
-        <Home />
-        <Juices />
-        {/* <Search /> */}
-        <Contact />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/juices" element={<Juices />} />
+          {/* Uncomment or add more routes as needed */}
+          {/* <Route path="/search" element={<Search />} /> */}
+          <Route path="/contact" element={<Contact />} />
+          {/* Add additional routes here */}
+        </Routes>
         <Footer />
       </JuiceProvider>
-    </div>
+    </Router>
   );
 }
 
